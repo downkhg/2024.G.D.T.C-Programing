3d5f46637126509d91450783536f5b588082b063
Administrator <Administrator@DESKTOP-4UNEB7C>
2025-01-02 13:24:52 +0900

    게임관리자를 이용한 GUI의 관리(FSM과 이벤트 처리)

--- Unity3D/FoxAdventrue/Basic/FoxAdventrue/Assets/Scripts/GameManager.cs
+++ Unity3D/FoxAdventrue/Basic/FoxAdventrue/Assets/Scripts/GameManager.cs
@@ -7,16 +7,81 @@ public class GameManager : MonoBehaviour
     public Responner resoponnerPlayer;
     public List<Responner> responnerMonsters;
 
+    public List<GameObject> listLifes;
     //public static int Life = 3; //게임관리자에 static을쓰더라도 플레이추가시마다 라이프가 추가되어야한다.
     public int Life = 3; //싱글톤을 이용하면 일반멤버도 다른객체에서 바로 접근가능하도록 만들수있다.
 
+    public void ProcessLife()
+    {
+        for (int i = 0; i < listLifes.Count; i++)
+        {
+            if (i < Life) listLifes[i].SetActive(true);
+            else listLifes[i].SetActive(false);
+        }
+    }
+
+    public List<GameObject> listGUIScenes;
+    public enum E_SCENE { PLAY, TITLE, THEEND, GAMEOVER, MAX }
+    public E_SCENE curScene;
+
+    public void ShowGUIState(E_SCENE scene)
+    {
+        for(E_SCENE idx = E_SCENE.PLAY; idx < E_SCENE.MAX; idx++)
+        {
+            if (idx == scene)
+                listGUIScenes[(int)idx].SetActive(true);
+            else
+                listGUIScenes[(int)idx].SetActive(false);
+        }
+    }
+
+    public void SetGUIState(E_SCENE scene)
+    {
+        switch (scene)
+        {
+            case E_SCENE.PLAY:
+                break;
+            case E_SCENE.TITLE:
+                break;
+            case E_SCENE.THEEND:
+                break;
+            case E_SCENE.GAMEOVER:
+                break;
+        }
+        ShowGUIState(scene);
+        curScene = scene;
+    }
+
+    public void UpdateGUIState()
+    {
+        switch (curScene)
+        {
+            case E_SCENE.PLAY:
+                break;
+            case E_SCENE.TITLE:
+                break;
+            case E_SCENE.THEEND:
+                break;
+            case E_SCENE.GAMEOVER:
+                break;
+        }
+    }
+
+    public void EventSenceChange(int idx)
+    {
+        SetGUIState((E_SCENE)idx);
+    }
+
     //싱글톤: 모든 클래스에서 게임관리자인스턴스에 접근하도록 만드는 패턴.//전역변수,정적멤버,객체의 참조
     public static GameManager GetInstance() { return instance;  }
+  
+
     public static GameManager instance = null;
 
     public void Awake()
     {
         instance = this;
+        SetGUIState(curScene);
     }
 
     public enum E_MONSTER{ OPPOSUM, EAGLE, FROG }
@@ -56,5 +121,7 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         ProcessSetPatrol();
+        ProcessLife();
+        UpdateGUIState();
     }
 }
