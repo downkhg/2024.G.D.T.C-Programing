990e92fab146ceaf984bf05ccc3ef4fb884fb33d
Administrator <Administrator@DESKTOP-4UNEB7C>
2025-01-02 11:50:50 +0900

    중찹상태를 해결하기위한 유한상태머신의 활용.

--- Unity3D/FoxAdventrue/Basic/FoxAdventrue/Assets/Scripts/Eagle.cs
+++ Unity3D/FoxAdventrue/Basic/FoxAdventrue/Assets/Scripts/Eagle.cs
@@ -6,36 +6,160 @@ public class Eagle : MonoBehaviour
 {
     public float Speed = 1;
     public float Site = 1;
-        
+
+    public Transform trTargetPoint;
+    public Transform trResponPoint;
+    public Transform trPatrolPoint;
+
+    public bool isPatrol;
+    public bool isReturn;
+    public bool isMove;
+    public bool isTracking;
+
+    public enum E_AI_STATUS
+    {
+        NONE,
+        TRACKING,
+        RETURN,
+        PATROL
     }
 
-    // Update is called once per frame
-    void Update()
+    public E_AI_STATUS curAIState = E_AI_STATUS.NONE;
+
+    public void SetState(E_AI_STATUS state)
     {
-        
+        switch(state)
+        {
+            case E_AI_STATUS.TRACKING:
+                
+                break;
+            case E_AI_STATUS.RETURN:
+                trTargetPoint = trResponPoint;
+                break;
+            case E_AI_STATUS.PATROL:
+                trTargetPoint = trPatrolPoint;
+                break;
+        }
+        curAIState = state;
+    }
+
+    public void UpdateState()
+    {
+        switch (curAIState)
+        {
+            case E_AI_STATUS.TRACKING:
+                if (trTargetPoint)
+                    SetState(E_AI_STATUS.RETURN);
+                break;
+            case E_AI_STATUS.RETURN:
+                if (trTargetPoint)
+                {
+                    if (isMove == false)
+                    {
+                        SetState(E_AI_STATUS.PATROL);
+                    }
+                }
+                break;
+            case E_AI_STATUS.PATROL:
+                if (trTargetPoint)
+                {
+                    if (isMove == false)
+                    {
+                        if (trTargetPoint.gameObject.name == trPatrolPoint.gameObject.name)
+                        {
+                            trTargetPoint = trResponPoint;
+                        }
+                        else if (trTargetPoint.gameObject.name == trResponPoint.gameObject.name)
+                        {
+                            trTargetPoint = trPatrolPoint;
+                        }
+                    }
+                }
+                break;
+        }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(this.transform.position, Site);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(trResponPoint.position, Time.deltaTime);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(trPatrolPoint.position, Time.deltaTime);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(trTargetPoint.position, Time.deltaTime*2);
     }
+
+    private void Start()
+    {
+        SetState(curAIState);
+    }
+
 
     private void FixedUpdate()
     {
+        FindProcess();
+        isMove = MoveProcess(trTargetPoint.position);
+        UpdateState();
+    }
+
+    public void FindProcess()
+    {
         Vector3 vPos = this.transform.position;
-        Collider2D collider = Physics2D.OverlapCircle(vPos, Site, 1<<LayerMask.NameToLayer("Player"));
+        Collider2D collider = Physics2D.OverlapCircle(vPos, Site, 1 << LayerMask.NameToLayer("Player"));
 
-        if(collider && collider.gameObject.tag == "Player")
+        if (collider)
         {
-            Debug.Log("OverlapCircle:" + collider.gameObject.name);
-            Vector3 vTargetPos = collider.transform.position;
-            Vector3 vDist = vTargetPos - vPos;//위치의 차이를 이용한 거리구하기
-            Vector3 vDir = vDist.normalized;//두물체사이의 방향(평준화-거리를뺀 이동량)
-            float fDist = vDist.magnitude; //두물체사이의 거리(스칼라-순수이동량)
-
-            if (fDist > Time.deltaTime)//한프레임의 이동거리보다 클때만 이동한다.
-                transform.position += vDir * Speed * Time.deltaTime;
+            trTargetPoint = collider.transform;
+            SetState(E_AI_STATUS.TRACKING);
         }
+    }
+
+    public void PatrolProcess()
+    {
+        if (trTargetPoint)
+        {
+            isMove = MoveProcess(trTargetPoint.position);
+            if (isMove == false)
+            {
+                if (isPatrol)
+                {
+                    if (trTargetPoint.gameObject.name == trPatrolPoint.gameObject.name)
+                    {
+                        trTargetPoint = trResponPoint;
+                    }
+                    else if (trTargetPoint.gameObject.name == trResponPoint.gameObject.name)
+                    {
+                        trTargetPoint = trPatrolPoint;
+                    }
+                }
+                else if (isReturn)
+                {
+                    isReturn = false;
+                    isPatrol = true;
+                    trTargetPoint = trPatrolPoint;
+                }
+            }
+        }
+    }
+
+    public bool MoveProcess(Vector3 vTargetPos)
+    {
+        Vector3 vPos = this.transform.position;
+        Vector3 vDist = vTargetPos - vPos;//위치의 차이를 이용한 거리구하기
+        Vector3 vDir = vDist.normalized;//두물체사이의 방향(평준화-거리를뺀 이동량)
+        float fDist = vDist.magnitude; //두물체사이의 거리(스칼라-순수이동량)
+
+        if (fDist > Time.deltaTime)//한프레임의 이동거리보다 클때만 이동한다.
+        {
+            transform.position += vDir * Speed * Time.deltaTime;
+            return true;
+        }
+        else
+            return false;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
