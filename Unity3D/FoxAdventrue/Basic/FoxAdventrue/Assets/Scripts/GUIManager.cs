a4d26b89ca696ac7eab68ab435be7d9d42a83537
Administrator <Administrator@DESKTOP-4UNEB7C>
2025-01-02 13:29:42 +0900

    관리자의 분리

---
+++ Unity3D/FoxAdventrue/Basic/FoxAdventrue/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManager : MonoBehaviour
+{
+    public List<GameObject> listGUIScenes;
+    public enum E_SCENE { PLAY, TITLE, THEEND, GAMEOVER, MAX }
+    public E_SCENE curScene;
+
+    public void Initialize()
+    {
+        SetGUIState(curScene);   
+    }
+
+    public void ShowGUIState(E_SCENE scene)
+    {
+        for (E_SCENE idx = E_SCENE.PLAY; idx < E_SCENE.MAX; idx++)
+        {
+            if (idx == scene)
+                listGUIScenes[(int)idx].SetActive(true);
+            else
+                listGUIScenes[(int)idx].SetActive(false);
+        }
+    }
+
+    public void SetGUIState(E_SCENE scene)
+    {
+        switch (scene)
+        {
+            case E_SCENE.PLAY:
+                Time.timeScale = 1;
+                break;
+            case E_SCENE.TITLE:
+                Time.timeScale = 0;
+                break;
+            case E_SCENE.THEEND:
+                Time.timeScale = 0;
+                break;
+            case E_SCENE.GAMEOVER:
+                Time.timeScale = 0;
+                break;
+        }
+        ShowGUIState(scene);
+        curScene = scene;
+    }
+
+    public void UpdateGUIState()
+    {
+        switch (curScene)
+        {
+            case E_SCENE.PLAY:
+                break;
+            case E_SCENE.TITLE:
+                break;
+            case E_SCENE.THEEND:
+                break;
+            case E_SCENE.GAMEOVER:
+                break;
+        }
+    }
+
+    public void EventSenceChange(int idx)
+    {
+        SetGUIState((E_SCENE)idx);
+    }
+}
--- Unity3D/FoxAdventrue/Basic/FoxAdventrue/Assets/Scripts/GameManager.cs
+++ Unity3D/FoxAdventrue/Basic/FoxAdventrue/Assets/Scripts/GameManager.cs
@@ -20,68 +20,19 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public List<GameObject> listGUIScenes;
-    public enum E_SCENE { PLAY, TITLE, THEEND, GAMEOVER, MAX }
-    public E_SCENE curScene;
-
-    public void ShowGUIState(E_SCENE scene)
-    {
-        for(E_SCENE idx = E_SCENE.PLAY; idx < E_SCENE.MAX; idx++)
-        {
-            if (idx == scene)
-                listGUIScenes[(int)idx].SetActive(true);
-            else
-                listGUIScenes[(int)idx].SetActive(false);
-        }
-    }
-
-    public void SetGUIState(E_SCENE scene)
-    {
-        switch (scene)
-        {
-            case E_SCENE.PLAY:
-                break;
-            case E_SCENE.TITLE:
-                break;
-            case E_SCENE.THEEND:
-                break;
-            case E_SCENE.GAMEOVER:
-                break;
-        }
-        ShowGUIState(scene);
-        curScene = scene;
-    }
-
-    public void UpdateGUIState()
-    {
-        switch (curScene)
-        {
-            case E_SCENE.PLAY:
-                break;
-            case E_SCENE.TITLE:
-                break;
-            case E_SCENE.THEEND:
-                break;
-            case E_SCENE.GAMEOVER:
-                break;
-        }
-    }
-
-    public void EventSenceChange(int idx)
-    {
-        SetGUIState((E_SCENE)idx);
-    }
-
     //싱글톤: 모든 클래스에서 게임관리자인스턴스에 접근하도록 만드는 패턴.//전역변수,정적멤버,객체의 참조
     public static GameManager GetInstance() { return instance;  }
   
 
     public static GameManager instance = null;
 
+
+    public GUIManager guiManager;
+
     public void Awake()
     {
         instance = this;
-        SetGUIState(curScene);
+        guiManager.Initialize();
     }
 
     public enum E_MONSTER{ OPPOSUM, EAGLE, FROG }
@@ -122,6 +73,6 @@ public class GameManager : MonoBehaviour
     {
         ProcessSetPatrol();
         ProcessLife();
-        UpdateGUIState();
+        guiManager.UpdateGUIState();
     }
 }
