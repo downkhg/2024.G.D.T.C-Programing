00cd795092fe6d18a533dcb42a60fb6292fbb7c7
Administrator <Administrator@DESKTOP-4UNEB7C>
2025-01-02 10:49:34 +0900

    현재 부족한 기능에 따라 추가적으로 기능을 정의하며 기획이나 코드를 추가한다.

--- Unity3D/FoxAdventrue/Basic/FoxAdventrue/Assets/Scripts/Dynamic.cs
+++ Unity3D/FoxAdventrue/Basic/FoxAdventrue/Assets/Scripts/Dynamic.cs
@@ -6,23 +6,55 @@ public class Dynamic : MonoBehaviour
 {
     public float JumpPower;
     public float Speed = 1;
+    public bool isGround;
+    public int Score;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-            transform.position += Vector3.right * Speed;// * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += Vector3.right * Speed * Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-            transform.position += Vector3.left * Speed;// * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position += Vector3.left * Speed * Time.deltaTime;
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
-            rigidbody.AddForce(Vector3.up * JumpPower);
+            if (isGround)
+            {
+                Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+                rigidbody.AddForce(Vector3.up * JumpPower);
+            }
         }
 
         //이동: 시간에 따라 위치가 변경되는 것.
         //transform.position += Vector3.right * Time.deltaTime;
+    }
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, 200, 20), "Score:" + Score);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name != "Ground")
+            Destroy(collision.gameObject);
+        if (collision.gameObject.name == "crate")
+            Score++;
+       
+        isGround = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        //땅을 벗어난 경우(땅에서 떨어질때도 작동하여 의도하지않은 코드를 만든다)
+        isGround = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "cherry")
+            Score += 10;
     }
 }
