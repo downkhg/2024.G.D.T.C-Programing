04ccea8e94e857b424979da83cacbb417c182130
Administrator <Administrator@DESKTOP-4UNEB7C>
2025-03-12 10:16:15 +0900

    지역변수 큐사용 및 arg를 통한 엑세스

---
+++ C++/OS/2.MFCApplication/2.MFCApplication/2.MFCApplication.h
@@ -0,0 +1,32 @@
+﻿
+// 2.MFCApplication.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CMy2MFCApplicationApp:
+// 이 클래스의 구현에 대해서는 2.MFCApplication.cpp을(를) 참조하세요.
+//
+
+class CMy2MFCApplicationApp : public CWinApp
+{
+public:
+	CMy2MFCApplicationApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMy2MFCApplicationApp theApp;
